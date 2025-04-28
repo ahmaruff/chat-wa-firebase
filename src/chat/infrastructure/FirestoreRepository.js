becf220e9af2e686ae901a8d10c8222be8cd11b7
Ahmad Ma'ruf <ahmadmaruf2701@gmail.com>
2025-04-28 10:01:50 +0700

    feat: init firebase repository chat

---
+++ src/chat/infrastructure/FirestoreRepository.js
@@ -0,0 +1,24 @@
+const admin = require('firebase-admin');
+const ChatRepository = require('../domain/repositories/ChatRepository');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const FIREBASE_CHAT_COLLECTION = process.env('FIREBASE_CHAT_COLLECTION') || 'wa_chat_test';
+
+class FirestoreRepository extends ChatRepository {
+  constructor() {
+    super();
+    this.db = admin.firestore();
+    this.chatCollection = this.db.collection(FIREBASE_CHAT_COLLECTION);
+  }
+
+  async save(chat){
+    try {
+      const chatDocRef = this.chatCollection.doc();
+    }catch(error) {
+      console.error('Error savingchat from Firestore:', error);
+      throw error;
+    }
+  }
+}
