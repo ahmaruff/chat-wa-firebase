fc55a8d435ca5d38f04cb89f0f873af84ec8e2bd
Ahmad Ma'ruf <ahmadmaruf2701@gmail.com>
2025-04-28 14:04:57 +0700

    fix: firebase config

--- src/shared/utils/firebase.js
+++ src/shared/utils/firebase.js
@@ -1,12 +1,16 @@
 const admin = require('firebase-admin');
 const config = require('./configs');
+const path = require('path');
 
 require('dotenv').config();
 
 const serviceAccountPath = config.firebase.service_account_path;
 
+const keyPath = path.resolve(process.cwd(), config.firebase.service_account_path);
+const serviceAccount = require(keyPath);
+
 admin.initializeApp({
-  credential: admin.credential.cert(require(serviceAccountPath)),
+  credential: admin.credential.cert(serviceAccount),
   databaseURL: config.firebase.database_url
 });
 
