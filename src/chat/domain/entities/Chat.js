f6215b2835e5df211ed229d19c11415a83a5f06e
Ahmad Ma'ruf <ahmadmaruf2701@gmail.com>
2025-04-28 11:45:40 +0700

    feat: update chat entity createdat to be nullable

--- src/chat/domain/entities/Chat.js
+++ src/chat/domain/entities/Chat.js
@@ -1,20 +1,19 @@
 const MessageContent = require('../valueObjects/MessageContent');
 
 class Chat {
-  constructor({id, sender, thread, messageContent, createdAt, unread = true}){
+  constructor({id, sender, thread, messageContent, createdAt = null, unread = true}){
     if (!sender) throw new Error('Chat must have a sender');
     if (!thread) throw new Error('Chat must have a thread');
     if (!(messageContent instanceof MessageContent)) {
       throw new Error('messageContent must be instance of MessageContent');
     }
-    if(!createdAt) throw new Error('Chat must have created_at timestamp');
 
     this.id = id || null;
     this.sender = sender;
     this.thread = thread || null;
     this.messageContent = messageContent;
     this.unread = unread;
-    this.createdAt = createdAt;
+    this.createdAt = createdAt || null;
   }
 
   toPrimitive() {
