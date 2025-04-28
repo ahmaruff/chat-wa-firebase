9b61cbdef64eed82d8915157947ee0491cb712c7
Ahmad Ma'ruf <ahmadmaruf2701@gmail.com>
2025-04-28 15:20:50 +0700

    fix: update chat controller, update last message

--- src/chat/interface/controllers/ChatController.js
+++ src/chat/interface/controllers/ChatController.js
@@ -1,5 +1,6 @@
 const SaveChatMessage = require('../../application/usecase/SaveChatMessage');
 const FindThreadByWaInfo = require('../../application/usecase/FindThreadByWaInfo');
+const FindThreadById = require('../../application/usecase/FindThreadById');
 const SaveThread = require('../../application/usecase/SaveThread');
 
 const ChatRepository = require('../../domain/repositories/ChatRepository');
@@ -10,6 +11,7 @@ const STATUS = require('../../../shared/constants/statusCodes');
 const responseFormatter = require('../../../shared/utils/responseFormatter');
 
 const config = require('../../../shared/utils/configs');
+const Thread = require('../../domain/entities/Thread');
 
 class ChatController {
   constructor(threadRepository, chatRepository) {
@@ -17,6 +19,7 @@ class ChatController {
     this.chatRepository = chatRepository;
     this.saveChatMessage = new SaveChatMessage(chatRepository);
     this.findThreadByWaInfo = new FindThreadByWaInfo(threadRepository);
+    this.findThreadById = new FindThreadById(threadRepository);
     this.saveThread = new SaveThread(threadRepository);
   }
 
@@ -61,8 +64,36 @@ class ChatController {
         threadId = existingThread.id;
       }
 
-      const chat = await this.saveChatMessage.execute(null, senderNumber, threadId, message, null, true);
-      res.status(200).json(responseFormatter(STATUS.SUCCESS, 200, 'chat saved successfully', {chat_id: chat.id}));
+      const chatId = await this.saveChatMessage.execute({
+        id: null,
+        sender: senderNumber,
+        thread: threadId,
+        message: messageText,
+        createdAt: null,
+        unread: true
+      });
+
+      if(chatId) {
+        const t = await this.findThreadById.execute(threadId);
+        console.log(t);
+        const tData = new Thread({
+          id: t.id,
+          contactName: t.contactName,
+          contactWaId: t.contactWaId || recipientNumber,
+          displayPhoneNumber: t.displayPhoneNumber,
+          startTime: t.startTime,
+          endTime: t.endTime,
+          lastMessage: messageText,
+          status: t.status,
+          waBusinessId: t.waBusinessId || waBusinessId,
+          lastUpdated: null
+        });
+        console.log(tData);
+
+        const updatedThread = await this.saveThread.execute(tData);
+      }
+
+      res.status(200).json(responseFormatter(STATUS.SUCCESS, 200, 'chat saved successfully', {chat_id: chatId}));
     } catch (error) {
       console.error('Error saving chat:', error);
       res.status(500).json(responseFormatter(STATUS.ERROR, 500, 'Failed to save chat', null));
