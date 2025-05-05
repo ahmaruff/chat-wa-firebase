39e2b82222477a44b144064c0342809d7a4e4a44
Ahmad Ma'ruf <ahmadmaruf2701@gmail.com>
2025-05-05 15:28:25 +0700

    feat: add WaConfig Repository

---
+++ src/channels/domain/repositories/WaConfigRepository.js
@@ -0,0 +1,32 @@
+class WaConfigRepository{
+  constructor(){
+  }
+
+  save(waConfig) {
+    throw new Error("Method save() must be implemented"); 
+  }
+
+  getById(id) {
+    throw new Error("Method getById() must be implemented"); 
+  }
+
+  getByChannelId(channelId) {
+    throw new Error("Method getByChannelId() must be implemented"); 
+  }
+
+  getByCrmChannelId(crmChannelId) {
+    throw new Error("Method getByCrmChannelId() must be implemented"); 
+  }
+
+  getAll(channelId) {
+    throw new Error("Method getAll() must be implemented"); 
+  }
+
+  getAllActive(channelId) {
+    throw new Error("Method getAllActive() must be implemented"); 
+  }
+
+  delete(id){
+    throw new Error("Method delete() must be implemented"); 
+  }
+}
