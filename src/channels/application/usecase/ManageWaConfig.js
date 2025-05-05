4d9d629a6b72faecb745d7c4ad21063c91dc3841
Ahmad Ma'ruf <ahmadmaruf2701@gmail.com>
2025-05-05 16:25:03 +0700

    feat: manage wa config

---
+++ src/channels/application/usecase/ManageWaConfig.js
@@ -0,0 +1,54 @@
+class ManageWaConfig{
+  constructor(waConfigRepository) {
+    this.waConfigRepository = waConfigRepository;
+  }
+
+  async save(waConfig) {
+    try {
+      const result = await this.waConfigRepository.save(waConfig);
+      return result;
+    } catch (error) {
+      console.error('Error save Wa config:', error);
+      throw error;
+    }
+  }
+
+  async getById(id) {
+    try {
+      const result = await this.waConfigRepository.getById(id);
+      if(!result) {
+        throw new Error(`Wa Config with ID ${id} not found`);
+      }
+
+      return result;
+    } catch (error) {
+      console.error('Error get by id Wa config:', error);
+      throw error;
+    }
+  }
+
+  async getByChannelId(channelId) {
+    try {
+      const result = await this.waConfigRepository.getByChannelId(channelId);
+
+      if(!result) {
+        throw new Error(`Wa Config with channel ID ${channelId} not found`);
+      }
+
+      return result;
+    } catch (error) {
+      console.error('Error get wa config by channel id:', error);
+      throw error;
+    }
+  }
+
+  async delete(id) {
+    try {
+      const result = await this.waConfigRepository.delete(id);
+      return result;
+    } catch (error) {
+      console.error('Error delete wa config:', error);
+      throw error;
+    }
+  }
+}
